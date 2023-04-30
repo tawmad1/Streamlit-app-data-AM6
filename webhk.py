d1a6ca4041bef890669f0fd583213f60ffc5ec9d
tawmad1 <83746276+tawmad1@users.noreply.github.com>
2023-04-30 07:21:06 +0200

    Add files via upload

--- webhk.py
+++ webhk.py
@@ -1,18 +1,27 @@
+import mysql.connector
+from flask import Flask, request
+
 app = Flask(__name__)
 
 # configure MySQL database connection
-cnx = mysql.connector.connect(user='admin', password='dSYhuuqh', host='mysql-122988-0.cloudclusters.net', database='mydatabase', port=10165)
+cnx = mysql.connector.connect(
+    user="admin",
+    password="h3yHNKqEn3220Xt8ccY9",
+    host="survey-cto-db.chintea7cswq.us-east-1.rds.amazonaws.com",
+    database="survey_cto_db",
+    port=3306,
+)
 cursor = cnx.cursor()
 
 # define webhook endpoint
-@app.route('/webhook', methods=['POST'])
+@app.route("/webhook", methods=["POST"])
 def handle_webhook():
     # parse the JSON data from the request body
     data = request.get_json()
 
     # extract the firstname and surname fields from the JSON data
-    firstname = data.get('firstname')
-    surname = data.get('surname')
+    firstname = data.get("firstname")
+    surname = data.get("surname")
 
     # insert the data into the MySQL database
     query = "INSERT INTO details (firstname, surname) VALUES (%s, %s)"
@@ -20,9 +29,17 @@ def handle_webhook():
     cursor.execute(query, values)
     cnx.commit()
 
+
     # return a response to the webhook
-    return 'Webhook received successfully!'
+    return "Webhook received successfully!"
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!'
+
 
 # start the Flask app and listen for incoming requests
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
+
+
